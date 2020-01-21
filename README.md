5fb86e291ec261ae5cb8f32f41882d822f6f0b53
Nathan Joubert <Joubz>
2020-01-21 22:52:30 +0100

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # RoadTrip_Parser
+
+
+Pour cloner le projet :
+Dans un répertoire : Git Bash : git clone https://github.com/Joubz/RoadTrip_Parser.git
+
+Pour voir les modifs : git status
+
+Pour add : git add 'nomFichier'
+
+pour push : git push
---
+++ src/Monument.java
@@ -0,0 +1,61 @@
+public class Monument {
+
+    String name;
+    String address;
+    String historic;
+    String hopeningHours;
+    String importance;
+
+    /**
+     * Constructeur
+     * @param name
+     */
+    Monument(String name)
+    {
+        this.name=name;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getHistoric() {
+        return historic;
+    }
+
+    public void setHistoric(String historic) {
+        this.historic = historic;
+    }
+
+    public String getHopeningHours() {
+        return hopeningHours;
+    }
+
+    public void setHopeningHours(String hopeningHours) {
+        this.hopeningHours = hopeningHours;
+    }
+
+    public String getImportance() {
+        return importance;
+    }
+
+    public void setImportance(String importance) {
+        this.importance = importance;
+    }
+}
